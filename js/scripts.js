fc6473ca4071dae8bb283676e31299211c26a01e
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-04-19 14:46:42 -0700

    Add functionality to remaining vocabulary words using jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,91 @@ $(document).ready(function() {
     $("#operators-definition").toggle();
     $("#operators-word").toggle();
   });
+
+  $(".card3").click(function() {
+    $("#variable-definition").toggle();
+    $("#variable-word").toggle();
+  });
+
+  $(".card4").click(function() {
+    $("#var-naming-conventions-definition").toggle();
+    $("#var-naming-conventions-word").toggle();
+  });
+
+  $(".card5").click(function() {
+    $("#functions-definition").toggle();
+    $("#functions-word").toggle();
+  });
+
+  $(".card6").click(function() {
+    $("#methods-definition").toggle();
+    $("#methods-word").toggle();
+  });
+
+  $(".card7").click(function() {
+    $("#arguments-definition").toggle();
+    $("#arguments-word").toggle();
+  });
+
+  $(".card8").click(function() {
+    $("#parameters-definition").toggle();
+    $("#parameters-word").toggle();
+  });
+
+  $(".card9").click(function() {
+    $("#return-definition").toggle();
+    $("#return-word").toggle();
+  });
+
+  $(".card10").click(function() {
+    $("#chaining-methods-definition").toggle();
+    $("#chaining-methods-word").toggle();
+  });
+
+  $(".card11").click(function() {
+    $("#strings-definition").toggle();
+    $("#strings-word").toggle();
+  });
+
+  $(".card12").click(function() {
+    $("#booleans-definition").toggle();
+    $("#booleans-word").toggle();
+  });
+
+  $(".card13").click(function() {
+    $("#undefined-definition").toggle();
+    $("#undefined-word").toggle();
+  });
+
+  $(".card14").click(function() {
+    $("#nan-definition").toggle();
+    $("#nan-word").toggle();
+  });
+
+  $(".card15").click(function() {
+    $("#null-definition").toggle();
+    $("#null-word").toggle();
+  });
+
+  $(".card16").click(function() {
+    $("#alert-definition").toggle();
+    $("#alert-word").toggle();
+  });
+
+  $(".card17").click(function() {
+    $("#comments-definition").toggle();
+    $("#comments-word").toggle();
+  });
+
+  $(".card18").click(function() {
+    $("#jquery-definition").toggle();
+    $("#jquery-word").toggle();
+  });
+
+  $(".card19").click(function() {
+    $("#attributes-definition").toggle();
+    $("#attributes-word").toggle();
+  });
+
 });
 
